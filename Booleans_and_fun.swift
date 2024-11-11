30c2939247fe05fa6593b7f54588c70e6778ed05
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 21:11:03 +0200

    Create Booleans_and_fun.swift

---
+++ Booleans_and_fun.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+let  IloveFootball = true // Boolean variable
+let  Ilovebasketball = true                                  
+
+// Simple conditional check
+if IloveFootball {
+    print("It is cool")
+} else {
+    print("it is cool")
+}
+let  IamThirtynineyearsold = true // Boolean variable
+let  Iamfourty = true                                  
+
+// Simple conditional check
+if IamThirtynineyearsold {
+    print("I can still learn new tricks")
+} else {
+    print("I can still learn new tricks")
+}
+let  friendshipiscool = true // Boolean variable
+let  loveiscool = true                                  
+
+// Simple conditional check
+if friendshipiscool {
+    print("let's have fun!!!")
+} else {
+    print("let's have fun")
+}
+let  printone = true // Boolean variable
+let  printtwo = true                                  
+
+// Simple conditional check
+if printone {
+    print("1")
+} else {
+    print("2")
+}
+let  printnewtricks = true // Boolean variable
+let  gettingold = false                                  
+
+// Simple conditional check
+if  printnewtricks {
+    print("new tricks")
+} else {
+    print("You can't learn new tricks")
+}
